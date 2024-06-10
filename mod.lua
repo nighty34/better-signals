5f16b3d032c0c0026d7d131f330e40525c30951e
nightfury34 <mc.nightfury34@gmail.com>
2024-06-10 17:27:56 +0200

    up default value of view distance

--- mod.lua
+++ mod.lua
@@ -13,7 +13,7 @@ function data()
 					uiType = "SLIDER",
 					values = { _("10"), _("20"), _("30"), _("40"), _("50"), _("60"), _("70"), _("80"), _("90"), _("100"), _("110"), _("120"), _("130"), _("140"), _("150"), _("160"), _("170"), _("180"), _("190"), _("200") },
 					tooltip = _("better_signals_view_distance_tooltip"),
-					defaultIndex = 2,
+					defaultIndex = 15,
 				  },
 			},
 			modid = "nightfury34_type-n_signale_1",
