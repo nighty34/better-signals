94980cb18c5e4e0bf8671da2f1c40568dfb05e84
nightfury34 <mc.nightfury34@gmail.com>
2025-05-04 14:13:42 +0200

    lower standard view distance

--- mod.lua
+++ mod.lua
@@ -23,7 +23,7 @@ function data()
 					uiType = "SLIDER",
 					values = { _("10"), _("20"), _("30"), _("40"), _("50"), _("60"), _("70"), _("80"), _("90"), _("100"), _("110"), _("120"), _("130"), _("140"), _("150"), _("160"), _("170"), _("180"), _("190"), _("200") },
 					tooltip = _("better_signals_view_distance_tooltip"),
-					defaultIndex = 15,
+					defaultIndex = 6,
 				  },
 				  {
 					key = "better_signals_debug_mode",
